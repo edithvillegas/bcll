ce6cf10dd6f0d793536c82237278181892d0b6ce
Edith Villegas <29732178+edithvillegas@users.noreply.github.com>
2026-05-05 15:54:26 +0000

    updated the prediction function

--- src/prediction.py
+++ src/prediction.py
@@ -7,7 +7,7 @@ def predict(patient):
 
     cluster=-1
 
-    if (patient['FISH_Del17'] >= 37):
+    if (patient['TP53'] >= 36):
             cluster = 7
     else:
         if patient['FISH_Del11'] >= 34:
@@ -19,7 +19,7 @@ def predict(patient):
                 if patient['IGHV_mutation'] >= 9:
                     cluster = 3
                 else:
-                    if patient['CD49d'] >= 51:
+                    if patient['CD49d'] >= 42 or patient['CD38'] >= 42:
                         cluster = 4
                     else:
                         if patient['IGHV_mutation'] >= 3:
